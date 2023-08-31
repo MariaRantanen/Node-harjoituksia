9b4a1fda15f1acdcb1a9c3dcf8930d3d9583739c
MariaRantanen <48141@edu.raseko.fi>
2023-08-31 10:21:18 +0300

    unohtuneet commitit

--- app.js
+++ app.js
@@ -27,19 +27,38 @@ app.set('view engine', 'handlebars');
 // URL ROUTES
 // ----------
 
-
-
+// Route to home page
 app.get('/', (req, res) => {
 
+    // Handlebars needs a key to show data on a page, json is a good way to send it
     let homePageData = {
         'price': 31.25,
         'wind': 2,
         'temperature': 18
-    }
+    };
+
     res.render('index', homePageData)
 
 });
 
+// Route to hourly data page
+app.get('/hourly',(req, res) => {
+
+    // Data will be presented in a table. To loop all rows we need a key for table and for column data
+    let hourlyPageData = { 'tabledata': [
+        {'hour': 13,
+        'price': 31.44},
+        {'hour': 14,
+        'price': 32.10},
+        {'hour': 15,
+        'price': 30.50},
+        {'hour': 16,
+        'price': 29.99},
+    ]
+    };
+
+    res.render('hourly', hourlyPageData)
+});
 // START THE LISTENER 
 app.listen(PORT);
 console.log('Server started and it will listen TCP port', PORT);
