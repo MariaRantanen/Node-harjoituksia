e4aa64e23d315e64a600b1e132fd8c0f93992ca1
rantanen maria <48141@edu.raseko.fi>
2023-09-22 15:41:45 +0300

    cron-schedule

---
+++ cron-test.js
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch';
+import cron from 'node-cron';
+
+
+const LATEST_PRICES_ENDPOINT = 'https://api.porssisahko.net/v1/latest-prices.json';
+
+async function fetchLatestPriceData() {
+  const response = await fetch(LATEST_PRICES_ENDPOINT);
+
+  return response.json();
+}
+
+function getPriceForDate(date, prices) {
+  const matchingPriceEntry = prices.find(
+    (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
+  );
+
+  if (!matchingPriceEntry) {
+    throw 'Price for the requested date is missing';
+  }
+
+  return matchingPriceEntry.price;
+}
+
+// Note that it's enough to call fetchLatestPriceData() once in 12 hours
+const { prices } = await fetchLatestPriceData();
+
+try {
+  const now = new Date();
+  const price = getPriceForDate(now, prices);
+  cron.schedule('1-5 * * * * *', () => {
+  console.log(`Hinta nyt (${now.toISOString()}): ${price} snt / kWh (sis. alv)`);
+  })}
+  catch (e) {
+  console.error(`Hinnan haku epäonnistui, syy: ${e}`);
+}
+
+
