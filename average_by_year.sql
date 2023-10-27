b4b178c3959b25dee1917ce2a637e2a5c0674651
rantanen maria <48141@edu.raseko.fi>
2023-10-27 11:25:39 +0300

    tää unohtu

--- average_by_year.sql
+++ average_by_year.sql
@@ -3,3 +3,15 @@ CREATE VIEW public.average_by_year AS
     avg(hourly_price.price) AS keskihinta
    FROM public.hourly_price
   GROUP BY (EXTRACT(year FROM hourly_price.timeslot));
+
+
+ALTER TABLE public.average_by_year OWNER TO postgres;
+
+--
+-- TOC entry 3383 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: VIEW average_by_year; Type: COMMENT; Schema: public; Owner: postgres
+--
+
+COMMENT ON VIEW public.average_by_year IS 'Average electricity prices on yearly basis';
+
